6914b3f75710fbe962f25562ea618fdc754f173e
Alexis Ferrer <alexisferrer60160@gmail.com>
2022-08-22 15:35:26 +0200

    modifications header et ajout du tri par catégorie

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,20 +6,20 @@ fetch("public/assets/js/statham.json")
     })
     .then(function (json) {
         json.results.forEach(element => {
-            let title = element.original_name;
-            let ref = element.id;
+            var title = element.original_name;
+            var ref = element.id;
             var cat = element.cat;
-            let color = element.color;
-            let description = element.description;
-            let overview = element.overview;
-            let housePrice = element.house_price;
-            let hotelPrice = element.hotel_price;
-            let bPrice = element.b_price;
-            let buttons = 0;
+            var color = element.color;
+            var description = element.description;
+            var overview = element.overview;
+            var housePrice = element.house_price;
+            var hotelPrice = element.hotel_price;
+            var bPrice = element.b_price;
+            var buttons = 0;
 
-            if (cat == "gare" || cat == "parc") {
+            if (cat == "parc") {
                 document.querySelector('.container').innerHTML +=
-                    `<div class="card">
+                    `<div class="card parc">
                     <div class="cont">
                         <div class="title ${color}">
                             <h2>${title}</h2>
@@ -41,9 +41,33 @@ fetch("public/assets/js/statham.json")
                     </div>
                 </div>`
             }
+            else if (cat == "gare") {
+                document.querySelector('.container').innerHTML +=
+                    `<div class="card gare">
+                        <div class="cont">
+                            <div class="title ${color}">
+                                <h2>${title}</h2>
+                            </div>
+                            <div class="imgs">
+                                <img src="${overview}" alt="photo de ${title}">
+                            </div>
+                            <h3>Prix d'achat</h3>
+                            <div class="content">
+                                <div class="houseBuy">
+                                    <p>${bPrice}€</p>
+                                    <button class="button-82-pushable" role="button">
+                                        <span class="button-82-shadow"></span>
+                                        <span class="button-82-edge"></span>
+                                        <span class="button-82-front text" data-street="${title}" data-price="${bPrice}">+</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+            }
             else {
                 document.querySelector('.container').innerHTML +=
-                    `<div class="card">
+                    `<div class="card rue">
                     <div class="cont">
                         <div class="title ${color}">
                             <h2>${title}</h2>
@@ -78,10 +102,32 @@ fetch("public/assets/js/statham.json")
     })
 
 document.getElementById('btnRue').addEventListener("click", ()=> {
-    if (cat == "rue"){
-        do
-    }
-}
+        document.querySelectorAll('.parc').forEach(element => {
+            element.classList.toggle('hide');
+        })
+        document.querySelectorAll('.gare').forEach(element => {
+            element.classList.toggle(('hide'));
+        })
+
+})
+
+document.getElementById('btnGare').addEventListener("click", ()=> {
+    document.querySelectorAll('.parc').forEach(element => {
+        element.classList.toggle('hide');
+    })
+    document.querySelectorAll('.rue').forEach(element => {
+        element.classList.toggle(('hide'));
+    })
+})
+
+document.getElementById('btnParc').addEventListener("click", ()=> {
+    document.querySelectorAll('.rue').forEach(element => {
+        element.classList.toggle('hide');
+    })
+    document.querySelectorAll('.gare').forEach(element => {
+        element.classList.toggle(('hide'));
+    })
+})
 
 
 // GESTION DU CAROUSEL
@@ -112,7 +158,7 @@ window.addEventListener("click", (e) => {
 
 
 
-// GESTION DE L'ONGLET DU PANIER
+// GESTION DE L'ONGvar DU PANIER
 
 cartButton.addEventListener("click", () => {
     cartSlide.classList.remove('cartSlideOut');
